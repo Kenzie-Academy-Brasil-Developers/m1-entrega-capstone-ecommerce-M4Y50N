ebb05dbd68fb6367244b22034997fa4591ca6625
M4Y50N <pethersonreis@gmail.com>
2022-09-21 21:52:02 -0300

    Search function implemented

--- js/index.js
+++ js/index.js
@@ -1,34 +1,3 @@
-//renderizar produtos no html
-let ul = document.querySelector(".ul__items");
-
-data.forEach((x) => {
-	let li = document.createElement("li");
-	li.classList.add("item");
-	li.classList.add("card");
-	li.classList.add("bg-gray-0");
-
-	li.innerHTML = `
-        <div class="card__head">
-            <img src="${x.img}" alt="${x.nameItem}" />
-        </div>
-        <div class="card__body bg-white">
-			<div>
-				<div class="clas small bg-primary fw-bold">${x.tag}</div>
-				<h3 class="title h3">${x.nameItem}</h3>
-			</div>
-            <p class="desc bodybold">
-                ${x.description}
-            </p>
-			<div>
-            	<p class="price fw-bold text-primary">R$ ${x.value}</p>
-            	<a id="p_${x.id}" class="add__cart bodybold" onclick="addItem(event.target)"> ${x.addCart} </a>
-			</div>
-        </div>
-    `;
-
-	ul.appendChild(li);
-});
-
 //add item in cart
 let carrinho = document.querySelector(".cart__content");
 
@@ -80,6 +49,11 @@ function rmItem(item) {
 	for (let i = 0; items.length > i; i++) {
 		if (items[i].id.substring(2) == item.id.substring(2)) {
 			carrinho.removeChild(item.parentNode.parentNode);
+			total -= data[item.id.substring(2)].value;
+			document.querySelector(".preco__total").innerHTML = total;
+
+			document.querySelector(".quantidade").innerHTML =
+				document.querySelector(".cart__content").childElementCount;
 			break;
 		}
 	}
@@ -89,3 +63,70 @@ function rmItem(item) {
 		carrinho.appendChild(vazio);
 	}
 }
+
+let resultSearch = [],
+	searchBar = document.getElementById("search__bar");
+
+function renderiza(element) {
+	let ul = document.querySelector(".ul__items");
+
+	let li = document.createElement("li");
+	li.classList.add("item");
+	li.classList.add("card");
+	li.classList.add("bg-gray-0");
+
+	li.innerHTML = `
+        <div class="card__head">
+            <img src="${element.img}" alt="${element.nameItem}" />
+        </div>
+        <div class="card__body bg-white">
+			<div>
+				<div class="clas small bg-primary fw-bold">${element.tag}</div>
+				<h3 class="title h3">${element.nameItem}</h3>
+			</div>
+            <p class="desc bodybold">
+                ${element.description}
+            </p>
+			<div>
+            	<p class="price fw-bold text-primary">R$ ${element.value}</p>
+            	<a id="p_${element.id}" class="add__cart bodybold" onclick="addItem(event.target)"> ${element.addCart} </a>
+			</div>
+        </div>
+    `;
+
+	ul.appendChild(li);
+}
+
+function showCard() {
+	let ul = document.querySelector(".ul__items");
+	//renderizar produtos no html
+	data.forEach((x) => {
+		if (searchBar.value == "") {
+			resultSearch.push(x);
+		} else if (
+			x.description.split(" ").filter((k) => {
+				return k.toLowerCase() == searchBar.value.toLowerCase();
+			}).length ||
+			x.nameItem.split(" ").filter((k) => {
+				return k.toLowerCase() == searchBar.value.toLowerCase();
+			}).length ||
+			x.tag.filter((k) => {
+				return k.toLowerCase() == searchBar.value.toLowerCase();
+			}).length
+		) {
+			resultSearch.push(x);
+		}
+	});
+
+	ul.innerHTML = "";
+
+	resultSearch.map((x) => {
+		renderiza(x);
+	});
+	resultSearch = [];
+}
+
+searchBar.addEventListener("keypress", showCard, false);
+searchBar.addEventListener("keyup", showCard, false);
+
+document.addEventListener("DOMContentLoaded", showCard, false);
